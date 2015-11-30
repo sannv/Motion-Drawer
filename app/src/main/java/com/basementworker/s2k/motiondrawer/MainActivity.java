35d1908d3f4c83d4687e7faec6a2c46973d60801
sannv <sannvoun@gmail.com>
2015-11-29 21:53:10 -0700

    Working draw app. Needs to add motion draw and make picking color work.

--- app/src/main/java/com/basementworker/s2k/motiondrawer/MainActivity.java
+++ app/src/main/java/com/basementworker/s2k/motiondrawer/MainActivity.java
@@ -1,16 +1,27 @@
 package com.basementworker.s2k.motiondrawer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawingView drawingView;
+    private Button currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawingView = (DrawingView)findViewById(R.id.drawing);
+        currPaint = (Button)findViewById(R.id.current_paint_color);
+//        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
     }
 
     @Override
@@ -33,5 +44,20 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+    The method will let the user choose a color with the color picker app
+    TODO set intent and get back color
+    TODO set the color to curr_paint. color should be a string #FFXXXXXX
+     */
+    public void paintClicked(View view){
+        currPaint = (Button)findViewById(R.id.current_paint_color);
+//        currPaint.setBackground("FF00FF00");
+        //fill in
+        int c = Color.parseColor("#FF00FF00");
+        drawingView.setColor("#FF00FF00");//this is here for testing
+        currPaint.setTag("#FF00FF00");
+        currPaint.setBackgroundColor(c);
     }
 }
