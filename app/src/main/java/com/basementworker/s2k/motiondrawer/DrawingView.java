4767ed2431912b52bce70ee6b9ea9ff50e3fc84b
sannv <sannvoun@gmail.com>
2015-11-30 17:40:01 -0700

    most of it is done

--- app/src/main/java/com/basementworker/s2k/motiondrawer/DrawingView.java
+++ app/src/main/java/com/basementworker/s2k/motiondrawer/DrawingView.java
@@ -6,26 +6,38 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by s2k on 11/16/2015.
  */
 
-public class DrawingView extends View{
+public class DrawingView extends View implements SensorEventListener{
     private Path drawPath;
     private Paint drawPaint, canvasPaint;
     private int paintColor = 0xFF660000;
     private Canvas drawCanvas;
     private Bitmap canvasBitmap;
+    private boolean useMotion, useBrush;
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private float motionX, motionY, touchX, touchY, newX, newY;
 
     public DrawingView(Context context, AttributeSet attrs){
         super(context, attrs);
         setupDrawing();
+        useMotion = false;
+        useBrush = true;
     }
 
     private void setupDrawing(){
@@ -56,32 +68,129 @@ public class DrawingView extends View{
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event){
-        float touchX = event.getX();
-        float touchY = event.getY();
+    public boolean onTouchEvent(MotionEvent event) {
+        touchX = event.getX();
+        touchY = event.getY();
+        motionX = getMotionX();
+        motionY = getMotionY();
 
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                drawPath.moveTo(touchX, touchY);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                drawPath.lineTo(touchX, touchY);
-                break;
-            case MotionEvent.ACTION_UP:
-                drawCanvas.drawPath(drawPath, drawPaint);
-                drawPath.reset();
-                break;
-            default:
-                return false;
+        if (useBrush == true) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    drawPath.moveTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    drawPath.lineTo(touchX, touchY);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    drawCanvas.drawPath(drawPath, drawPaint);
+                    drawPath.reset();
+                    break;
+                default:
+                    return false;
+            }
+
         }
+        if(useMotion == true) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touchX = event.getX();
+                    touchY = event.getY();
+                    drawPath.moveTo(motionX, motionY);
+//                    drawPath.reset();
+                    drawPath.lineTo(motionX, motionY);
+                    drawing();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    drawCanvas.drawPath(drawPath, drawPaint);
+                    drawPath.reset();
+                    break;
+                default:
+                    return false;
+            }
+        }
+
         invalidate();
         return true;
+    }
+
+    public void onSensorChange(float x, float y) {
+        if(useMotion==true) {
+                motionX = x;
+                motionY = y;
+                newX = touchX + motionX ;
+                newY = touchY + motionY ;
+                drawing();
+            setColor("#FF0000FF");
+
+        }
+    }
+
+
+    public void drawing(){
+        drawPath.moveTo(touchX, touchY);
+        drawPath.moveTo(newX, newY);
+        drawCanvas.drawPath(drawPath, drawPaint);
+        touchX =+newX;
+        touchY =+newY;
+//        drawPath.reset();
+
+        invalidate();
+    }
+
+    public float getMotionX(){
+        return motionX;
+    }
+
+    public float getMotionY(){
+        return motionY;
+    }
+
+    public void setUpSensor(SensorManager senMan, Sensor senAcc){
+        senSensorManager = senMan;
+        senAccelerometer = senAcc;
+    }
+
+    public void useMotionDraw(){
+        invalidate();
+        useMotion = true;
+        useBrush = false;
+
+    }
+
+    public void useBrushDraw(){
+        invalidate();
+        useBrush = true;
+        useMotion = false;
+
     }
 
     public void setColor(String newColor){
         invalidate();
         paintColor = Color.parseColor(newColor);
         drawPaint.setColor(paintColor);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+//        Sensor mySensor = sensorEvent.sensor;
+        motionX = event.values[0];
+        motionY = event.values[1];
+        float newX;
+        float newY;
+//            if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//                motionX = event.values[0];
+//                motionY = event.values[1];
+        newX = event.values[0];
+        newY = event.values[1];
+        if(Math.abs(newX - motionX) >= 0 || Math.abs(newY - motionY) > 0) {
+            onSensorChange(motionX, motionY);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
 }
--- app/src/main/java/com/basementworker/s2k/motiondrawer/MainActivity.java
+++ app/src/main/java/com/basementworker/s2k/motiondrawer/MainActivity.java
@@ -1,6 +1,14 @@
 package com.basementworker.s2k.motiondrawer;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,10 +17,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
     private DrawingView drawingView;
     private Button currPaint;
+    private Button motionDraw, brushDraw, saveButton;
+    private int rad = 0;
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private float motionX, motionY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +36,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         drawingView = (DrawingView)findViewById(R.id.drawing);
         currPaint = (Button)findViewById(R.id.current_paint_color);
-//        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+        motionDraw = (Button)findViewById(R.id.motion_paint_btn);
+        motionDraw.setOnClickListener(this);
+        brushDraw = (Button)findViewById(R.id.draw_btn);
+        brushDraw.setOnClickListener(this);
+        saveButton = (Button)findViewById(R.id.save_btn);
+        saveButton.setOnClickListener(this);
+        senSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        drawingView.setUpSensor(senSensorManager, senAccelerometer);
+    }
 
+    @Override
+    public void onClick( View view ){
+        if(view.getId()==R.id.motion_paint_btn){
+            drawingView.useMotionDraw();
+        }
+        else if(view.getId()==R.id.draw_btn){
+            drawingView.useBrushDraw();
+        }
+        else if(view.getId()==R.id.save_btn){
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to device Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    //save drawing
+                    drawingView.setDrawingCacheEnabled(true);
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), drawingView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+                    if(imgSaved!=null){
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else{
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                    drawingView.destroyDrawingCache();
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
+        }
     }
 
     @Override
@@ -52,12 +117,57 @@ public class MainActivity extends AppCompatActivity {
     TODO set the color to curr_paint. color should be a string #FFXXXXXX
      */
     public void paintClicked(View view){
-        currPaint = (Button)findViewById(R.id.current_paint_color);
+        if(rad == 0) {
+            currPaint = (Button) findViewById(R.id.current_paint_color);
 //        currPaint.setBackground("FF00FF00");
-        //fill in
-        int c = Color.parseColor("#FF00FF00");
-        drawingView.setColor("#FF00FF00");//this is here for testing
-        currPaint.setTag("#FF00FF00");
-        currPaint.setBackgroundColor(c);
+            //fill in
+            int c = Color.parseColor("#FF00FF00");
+            drawingView.setColor("#FF00FF00");//this is here for testing
+            currPaint.setTag("#FF00FF00");
+            currPaint.setBackgroundColor(c);
+            rad = 1;
+        }
+        else if(rad == 1){
+            drawingView.setColor("#FF000000");
+            currPaint.setTag("#FF000000");
+            currPaint.setBackgroundColor((int)Color.parseColor("#FF000000"));
+            rad = 0;
+        }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+//        Sensor mySensor = sensorEvent.sensor;
+        motionX = event.values[0];
+        motionY = event.values[1];
+        float newX;
+        float newY;
+//            if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//                motionX = event.values[0];
+//                motionY = event.values[1];
+        newX = event.values[0];
+        newY = event.values[1];
+                if(Math.abs(newX - motionX) >= 0 || Math.abs(newY - motionY) > 0) {
+                    drawingView.onSensorChange(motionX, motionY);
+                }
+
+//        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
 }
